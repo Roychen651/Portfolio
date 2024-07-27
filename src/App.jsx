1275b4c052c013b547c4978c1975537eb42b2945
Roy Chen <roy97chen@gmail.com>
2024-07-28 00:00:25 +0300

    finish hero and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import "./app.scss";
+import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Test from "./Test";
 
 const App = () => {
   return <div>
 
-    {/* <section>
+    <section id="Homepage">
       <Navbar/>
+      <Hero/>
     </section>
-    <section>Parallax</section>
-    <section>Services</section>
-    <section>Parallax</section>
+    <section id="Skills">Parallax</section>
+    <section>Skills</section>
+    <section id="Portfolio">Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
     <section>Portfolio4</section>
     <section>Portfolio5</section>
     <section>Portfolio6</section>
-    <section>Contact</section> */}
+    <section id="Contact">Contact</section>
 
     <Test/>
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import './Navbar.scss'
+import React from "react";
+import "./Navbar.scss";
+import { motion } from "framer-motion";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        
-        <div className="wrapper">
-            <span>Roy Chen</span>
-            <div className="social">
-                <a href="https://www.facebook.com/roy.chen97/"><img src="/facebook.png" alt="" /></a>
-                <a href="https://www.linkedin.com/in/roychen651/"><img src="/linkedin-icon.png" alt="" /></a>
-                <a href="https://www.instagram.com/roychen.97"><img src="/instagram.png" alt="" /></a>
-                <a href="https://github.com/Roychen651"><img src="/github.png" alt="" /></a>
-
-            </div>
+    <div className="navbar">
+        <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Roy Chen
+        </motion.span>
+        <div className="social">
+          <a href="https://www.facebook.com/roy.chen97/">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/roychen651/">
+            <img src="/linkedin-icon.png" alt="" />
+          </a>
+          <a href="https://www.instagram.com/roychen.97">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="https://github.com/Roychen651">
+            <img src="/github.png" alt="" />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
