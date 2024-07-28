88591281900aaeb7e308f368671b8aee68c4a7ca
Roy Chen <roy97chen@gmail.com>
2024-07-28 11:17:48 +0300

    finish parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Parallax from "./Components/Parallax/Parallax";
 import Test from "./Test";
 
 const App = () => {
@@ -10,9 +11,9 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Skills">Parallax</section>
+    <section id="Skills"><Parallax type="skills"/></section>
     <section>Skills</section>
-    <section id="Portfolio">Parallax</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
